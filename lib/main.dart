c2707df8b4d1a761c832ea7f1d6d4052a8b14802
leandroalves <leandro.alves@lighthouseit.com.br>
2022-10-12 09:05:33 -0300

    List photos

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,30 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:hiperbank/services/getPhotos.dart';
 
-// List<String>.generate(1000, (Item) => "Item $i");
-
 void main() async {
-  var photos = await getPhotos();
+  var response = await getPhotos();
+  var photos = jsonDecode(response);
+
+  BuildListViwer() {
+    return ListView.builder(
+      itemCount: photos.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          // leading: Image(image: AssetImage(photos[index]['thumbnailUrl'])),
+          leading: Image.network(photos[index]['thumbnailUrl']),
+          title: Text('${photos[index]['title']}'),
+        );
+      },
+    );
+  }
+
   runApp(MaterialApp(
     home: Scaffold(
         appBar: AppBar(
           title: Text('Minha Primeira App'),
         ),
-        body: Text(photos)),
+        body: BuildListViwer()),
   ));
 }
